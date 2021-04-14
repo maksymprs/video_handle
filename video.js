ab259b9969bc15bb435314a8ce817e1ff50e655c
maksymprs <max.prs@gmail.com>
2021-04-14 17:59:17 +0300

    Vladimir's corrections

--- video.js
+++ video.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const playList = fs.readFileSync('./video_1.m3u8', 'utf-8');
-let chunksList = [];
-for (let i =0; i < playList.length - 30; i++) {
-    if (playList.substring(i, i + 5) == "video") {
-        chunksList.push(playList.substring(i, i + 21));
-    }
-}
-console.log(chunksList[chunksList.length - 1]);
+const segments = playList
+  .split('\n')
+  .filter(line => line.slice(-3) === '.ts');
+
+//https://storage.yandexcloud.net/kabinetsokalskaya/video/Free/ResursPsy.mp4/
+
+console.log(segments);
